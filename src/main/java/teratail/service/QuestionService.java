834555197962a0612a0757faf6b9a019d128f252
yohira0616 <yukito.sys.so@gmail.com>
2016-11-17 01:11:26 +0900

    mainメソッド削除

--- src/main/java/teratail/service/QuestionService.java
+++ src/main/java/teratail/service/QuestionService.java
@@ -2,22 +2,16 @@ package teratail.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import teratail.common.ResultEntityParser;
 import teratail.common.TeratailHost;
-import teratail.model.Meta;
 import teratail.model.response.QuestionEntity;
 import teratail.model.response.QuestionListEntity;
 import teratail.service.spec.QuestionServiceSpec;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class QuestionService implements QuestionServiceSpec {
 
@@ -33,28 +27,6 @@ public class QuestionService implements QuestionServiceSpec {
 
   public QuestionService(String accessToken) {
     this.accessToken = accessToken;
-  }
-
-  public static void main(String... args) {
-
-    try {
-
-      List<Header> headers = new ArrayList<Header>();
-      HttpClient httpClient = HttpClientBuilder.create()
-          .setDefaultHeaders(headers).build();
-      HttpGet httpGet = new HttpGet(TeratailHost.HOST + "questions/55430");
-      HttpResponse response = httpClient.execute(httpGet);
-      //String body = EntityUtils.toString(response.getEntity(), "UTF-8");
-      //System.out.println(body);
-
-      Meta meta = ResultEntityParser.parseMeta(response.getEntity());
-      System.out.println(meta);
-
-
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException();
-    }
   }
 
   @Override
